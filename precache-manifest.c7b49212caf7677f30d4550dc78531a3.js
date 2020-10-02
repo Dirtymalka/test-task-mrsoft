9fa0ac026227036387fa42faa912c1a535e8a48b
Artem <artem.antonov1993@gmail.com>
2020-10-02 22:49:48 +0300

    Updates

---
+++ precache-manifest.c7b49212caf7677f30d4550dc78531a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94f3ba3479f4d329b26e12621338a78b",
+    "url": "/test-task-mrsoft/index.html"
+  },
+  {
+    "revision": "571140b96f15ab3a9563",
+    "url": "/test-task-mrsoft/static/css/main.779c6e46.chunk.css"
+  },
+  {
+    "revision": "2bacde5f2899e0b7858b",
+    "url": "/test-task-mrsoft/static/js/2.04ffb446.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-task-mrsoft/static/js/2.04ffb446.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "571140b96f15ab3a9563",
+    "url": "/test-task-mrsoft/static/js/main.6e8bf236.chunk.js"
+  },
+  {
+    "revision": "dd2d79e5a8a91a442d8d",
+    "url": "/test-task-mrsoft/static/js/runtime-main.d8d3956a.js"
+  }
+]);
