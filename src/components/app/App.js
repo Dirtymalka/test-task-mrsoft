e74f4015cb2c3d18b4753d189cba25e89ce30a61
Artem <artem.antonov1993@gmail.com>
2020-10-03 11:56:33 +0300

    refactor: fix the problem with cors  policy

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -8,12 +8,11 @@ function App() {
   const [filterStrings, setFilterStrings] = useState([]);
   const [inputValue, setInputValue] = useState('');
   const [isCaseRegister, setIsCaseRegister] = useState(true);
-  const [error, setError] = useState(true);
+  const [error, setError] = useState(false);
 
   const getStrings = async () => {
-    const url = `https://www.mrsoft.by/data.json`;
+    const url = `https://cors-anywhere.herokuapp.com/https://www.mrsoft.by/data.json`;
     const response = await fetch(url);
-
     if (response.ok) {
       const result = await response.json();
       setDataStrings(result.data);
@@ -40,6 +39,9 @@ function App() {
   }
 
   const filterOnSubstring = () => {
+    if (!isNaN(inputValue) && inputValue !== '') {
+      return;
+    }
     const newDataStrings = [...dataStrings];
     if (isCaseRegister) {
       const filterDataStrings = newDataStrings.filter((string) => string.includes(inputValue));
@@ -62,23 +64,26 @@ function App() {
     setFilterStrings(dataStrings)
   }, [dataStrings])
 
-  if (!dataStrings.length) {
-    return <Spinner />
-  }
 
   if (error) {
     return <ErrorIndicator />
   }
 
+  if (!dataStrings.length) {
+    return <Spinner />
+  }
+
   return (
     <div className="container my-container">
-      <input className="form-control" type="text" onChange={handleInputChange} />
-      <div className="form-group form-check">
-        <input type="checkbox" className="form-check-input" id="exampleCheck1" checked={isCaseRegister} onChange={handleCheckBox} />
-        <label className="form-check-label" htmlFor="exampleCheck1">Apply register?</label>
-      </div>
-      <button className="btn btn-primary button" onClick={filterOnLength}>Show strings with length more {!isNaN(inputValue) ? inputValue : null}</button>
-      <button className="btn btn-primary button" onClick={filterOnSubstring}>Show strings with substring '{isNaN(inputValue) ? inputValue : null}'</button>
+      <header className="header">
+        <input className="form-control" type="text" onChange={handleInputChange} />
+        <div className="form-group form-check">
+          <input type="checkbox" className="form-check-input" id="exampleCheck1" checked={isCaseRegister} onChange={handleCheckBox} />
+          <label className="form-check-label" htmlFor="exampleCheck1">Apply register?</label>
+        </div>
+        <button className="btn btn-primary button" onClick={filterOnLength}>Show strings with length more {!isNaN(inputValue) ? inputValue : null}</button>
+        <button className="btn btn-primary button" onClick={filterOnSubstring}>Show strings with substring "{isNaN(inputValue) ? inputValue : null}"</button>
+      </header>
       <ul className="list-group list-group-flush list-strings">
         {filterStrings.map((string, idx) => <li className="list-group-item" key={idx}>{string}</li>)}
       </ul>
